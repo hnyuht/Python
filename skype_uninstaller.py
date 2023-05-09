e9b4cc10f67b5f2ffc797b5f27d1fa82194eb6b6
hnyuht <hnyuht@gmail.com>
2023-05-08 20:23:34 -0600

    Update skype_uninstaller.py

--- skype_uninstaller.py
+++ skype_uninstaller.py
@@ -3,12 +3,12 @@ import os
 
 def uninstall_skype():
     try:
-        # Specify the Skype package full name
-        package_full_name = "Microsoft.SkypeApp_15.97.3204.0_x64__kzf8qxf38zg5c"
+        # Specify the Skype package name or wildcard pattern
+        package_name = "Microsoft.SkypeApp*"
 
         # Execute the PowerShell command to uninstall the Skype app
         subprocess.run(['C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe',
-                        '-Command', f'Get-AppxPackage -AllUsers | Where-Object {{$_.PackageFullName -like "*{package_full_name}*"}} | ForEach-Object {{Remove-AppxPackage -Package $_.PackageFullName}}'],
+                        '-Command', f'Get-AppxPackage -AllUsers | Where-Object {{$_.Name -like "{package_name}"}} | ForEach-Object {{Remove-AppxPackage -Package $_.PackageFullName}}'],
                        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
         # Specify the path to the Skype uninstaller
